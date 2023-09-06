8eede5c91cf6ed85ff1e1287865ca7edb2f457af
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-09-07 00:25:01 +0530

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -61,7 +61,7 @@ async def close_admin_callback(_, q: CallbackQuery):
 
 
 @Gojo.on_message(
-    command("start") & (filters.group | filters.private),
+    command("mstart") & (filters.group | filters.private),
 )
 async def start(c: Gojo, m: Message):
 
@@ -105,7 +105,7 @@ async def start(c: Gojo, m: Message):
                     return
         try:
             cpt = f"""
-Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am Gojo ✨.
+Hey [{m.from_user.first_name}](http://t.me/{m.from_user.username})! I am management bot ✨.
 I'm here to help you manage your groups!
 Hit /help to find out more about how to use me in my full potential!
 
@@ -144,7 +144,7 @@ Join my [News Channel](https://t.me/gojo_bots_network) to get information on all
 async def start_back(_, q: CallbackQuery):
     try:
         cpt = f"""
-Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Gojo ✨.
+Hey [{q.from_user.first_name}](http://t.me/{q.from_user.username})! I am Management Bot.
 I'm here to help you manage your groups!
 Hit /help to find out more about how to use me in my full potential!
 
@@ -166,7 +166,7 @@ async def commands_menu(_, q: CallbackQuery):
     keyboard = ikb(ou, True)
     try:
         cpt = f"""
-Hey **[{q.from_user.first_name}](http://t.me/{q.from_user.username})**! I am Gojo✨.
+Hey **[{q.from_user.first_name}](http://t.me/{q.from_user.username})**! I am Management Bot.
 I'm here to help you manage your groups!
 Commands available:
 × /start: Start the bot
